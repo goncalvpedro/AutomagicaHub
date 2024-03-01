599280812af4a91c2c3eb680deda0d42e9f26de9
Pedro Gonçalves <pedrogoncalves.ie@gmail.com>
2024-03-01 20:54:24 -0300

    Merge and rotation are good to work

--- Extract Data from PDF/file_explorer.py
+++ Extract Data from PDF/file_explorer.py
@@ -2,9 +2,19 @@ import tkinter as tk
 from tkinter import filedialog
 
 def select_files():
-    root = tk.Tk()
-    root.withdraw()
     file_paths = filedialog.askopenfilenames()
-
     return file_paths
 
+def get_file_info():
+    files = select_files()
+    file_list = []
+    
+    for file in files:
+        file_name = file.split('/')[-1].split('.')[0]
+        info = (file, file_name)
+        file_list.append(info)
+    
+    return file_list
+
+root = tk.Tk()
+root.withdraw()
--- Extract Data from PDF/pdf_reader.py
+++ Extract Data from PDF/pdf_reader.py
@@ -1,33 +1,61 @@
 from PyPDF2 import PdfReader, PdfWriter
-import file_explorer 
+import file_explorer
+file_names = ''
 
-file_paths = list(file_explorer.select_files())
-file_names = []
-
-for file in file_paths:
-    file_names.append(file.split("/")[-1].split(".")[0])
-
-def mergePdf(file_paths):
+def mergePdf():
+    file_paths = file_explorer.get_file_info()
     writer = PdfWriter()
-    for file_path in file_paths:
-        reader = PdfReader(file_path)
+
+    for item in file_paths:
+        reader = PdfReader(item[0])
         for page in reader.pages:
             writer.add_page(page)
-    writer.write(f"{file_names[0]}_merged.pdf")
+    writer.write(f"{file_paths[0][1]}_merged.pdf")
+    return print('Check your directory folder to see the merged file.')
 
+def rotatePdf():
+    file_paths = file_explorer.get_file_info()
 
-def rotatePdf(file_path, degrees):
-    reader = PdfReader(file_path[0])
+    if file_paths is False:
+        print('You need to select a pdf file')
+        return
+
+    if len(file_paths) > 1:
+        print('You can only rotate one pdf at a time')
+        return
+
+    try:
+        degrees = float(input('Type the degrees you want to rotate the pdf: '))
+        if degrees > 360:
+            print('The degrees must be lower than 360°')
+            return
+        elif degrees < 0:
+            print('The degrees must be higher than 0°')
+            return
+        elif degrees%90 != 0:
+            print('The degrees must be a multiple of 90°')
+            return
+    except:
+        print('You need to type a number')
+        rotatePdf()
+    
+    reader = PdfReader(file_paths[0][0])
     writer = PdfWriter()
+
     for page in reader.pages:
         page.rotate(degrees)
         writer.add_page(page)
-    writer.write(f"{file_names[0]}_rotated.pdf")
+    writer.write(f"{file_paths[0][1]}_rotated.pdf")
+    return print('Check your directory folder to see the rotated file.')
 
+def extractText():
+    file_paths = list(file_explorer.select_files())
 
-def extractText(file_path):
-    for file in file_path:
-        with open(file_path[file], "rb") as f:
+    for file in file_paths:
+        file_names.append(file.split("/")[-1].split(".")[0])
+
+    for file in file_paths:
+        with open(file_paths[file], "rb") as f:
             reader = PdfReader(f)
             text = ""
             for page in reader.pages:
@@ -35,20 +63,28 @@ def extractText(file_path):
     return text
 
 
-def extractImages(file_path):
-    for file in file_path:
-        with open(file_path[file], "rb") as f:
+def extractImages():
+    file_paths = list(file_explorer.select_files())
+
+    for file in file_paths:
+        file_names.append(file.split("/")[-1].split(".")[0])
+
+    for file in file_paths:
+        with open(file_paths[file], "rb") as f:
             reader = PdfReader(f)
             images = []
             for page in reader.pages:
                 images.append(page.images)
     return images
 
-def reducePdf(file_path):
-    count = 0
+def resizePdf():
+    file_paths = list(file_explorer.select_files())
 
-    for file in file_path:
-        reader = PdfReader(file_path[count])
+    for file in file_paths:
+        file_names.append(file.split("/")[-1].split(".")[0])
+
+    for file in file_paths:
+        reader = PdfReader(file_paths[count])
         writer = PdfWriter()
 
         for page in reader.pages:
@@ -62,5 +98,60 @@ def reducePdf(file_path):
         
         count += 1
 
+def encryptPdf():
+    while True:
+        password = input('Type a password')
+        confirm = input('Confirm password')
 
+        if password != confirm:
+            print('Passwords do not match')
+        elif password == confirm:
+            break
+            
+    file_paths = list(file_explorer.select_files())
+
+    for file in file_paths:
+        file_names.append(file.split("/")[-1].split(".")[0])
+
+    for file in file_paths:
+        reader = PdfReader(file_paths[count])
+        writer = PdfWriter()
+
+        for page in reader.pages:
+            writer.add_page(page)
+
+        writer.encrypt(password)
+
+        with open(f'{file_names[count]}_encrypted.pdf', "wb") as fp:
+            writer.write(fp)
+        count += 1
+
+def decryptPdf():
+    file_paths = list(file_explorer.select_files())
+
+    for file in file_paths:
+        file_names.append(file.split("/")[-1].split(".")[0])
+        
+    while True:
+        password = input('Type a password')
+        confirm = input('Confirm password')
+
+        if password != confirm:
+            print('Passwords do not match')
+        elif password == confirm:
+            break
+            
+
+    for file in file_paths:
+        reader = PdfReader(file_paths[count])
+        writer = PdfWriter()
+
+        for page in reader.pages:
+            writer.add_page(page)
+
+        writer.decrypt(password)
+
+        with open(f'{file_names[count]}_decrypted.pdf', "wb") as fp:
+            writer.write(fp)
+        count += 1
 
